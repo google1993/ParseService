de0b630da0c041b887b29915aa57399dfcbe1398
Khatuncev Georgy <google_12345@mail.ru>
2018-01-23 09:15:23 +0500

    Парсим страницу. Результат записываем в SQL

---
+++ Elements/Elements.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParseService.Elements
+{
+    class Process
+    {
+
+        public int ProcessId { get; set; }
+        public int? PrefixPlav { get; set; }
+        public int? NomerPlav { get; set; }
+        public int Pech { get; set; }
+        [NotMapped]
+        public string Plavka
+        {
+            get
+            {
+                if (!PrefixPlav.HasValue || !NomerPlav.HasValue)
+                    return Pech.ToString("D2");
+                else
+                {
+                    int nomer = (int)NomerPlav;
+                    return PrefixPlav.ToString() + "-" + Pech.ToString("D2") + "-" + nomer.ToString("D5");
+                }
+            }
+            set
+            {
+                if (int.TryParse(value, out int param))
+                {
+                    Pech = param;
+                    return;
+                }
+                if (value.Length <= 6)
+                {
+                    int.TryParse(value.Substring(2, 2), out param);
+                    Pech = param;
+                    return;
+                }
+                if (int.TryParse(value.Substring(0, 1), out param))
+                    PrefixPlav = param;
+                else
+                    PrefixPlav = null;
+                Pech = int.TryParse(value.Substring(2, 2), out param) ? param : 0;
+                if (int.TryParse(value.Substring(5, 5), out param))
+                    NomerPlav = param;
+                else
+                    NomerPlav = null;
+            }
+        }
+
+        public string Splav { get; set; }
+        public string Ukazanie { get; set; }
+        public string Contract { get; set; }
+
+        public int NaborOg { get; set; }
+        public int NaborPer { get; set; }
+        [NotMapped]
+        public string Nabor
+        {
+            get
+            {
+                return NaborOg.ToString() + "-" + NaborPer.ToString();
+            }
+            set
+            {
+                if (int.TryParse(value.Substring(0, value.IndexOf('-')), out int param))
+                    NaborOg = param;
+                if (int.TryParse(value.Substring(value.IndexOf('-') + 1), out param))
+                    NaborPer = param;
+            }
+        }
+
+        public int ComplectNum { get; set; }
+        public int ComplectIzl { get; set; }
+        public int ComplectDiam { get; set; }
+        [NotMapped]
+        public string Complect
+        {
+            get
+            {
+                return ComplectNum.ToString() + "/" + ComplectIzl.ToString() + "-" + ComplectDiam.ToString();
+            }
+            set
+            {
+                if (int.TryParse(value.Substring(0, value.IndexOf('/')), out int param))
+                    ComplectNum = param;
+                if (int.TryParse(value.Substring(value.IndexOf('/') + 1, value.IndexOf('-') - (value.IndexOf('/') + 1)), out param))
+                    ComplectIzl = param;
+                if (int.TryParse(value.Substring(value.IndexOf('-') + 1), out param))
+                    ComplectDiam = param;
+            }
+        }
+
+        public int ElectVes { get; set; }
+        public string Operation { get; set; }
+        public bool OperFlag { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime? EndTeor { get; set; }
+        [NotMapped]
+        public string EndTeorStr
+        {
+            set
+            {
+                if (value == "-") {
+                    EndTeor = null;
+                    return;
+                }
+                int.TryParse(value.Substring(0, value.IndexOf(':')), out int hour);
+                int.TryParse(value.Substring(value.IndexOf(':') + 1), out int min);
+                TimeSpan t = new TimeSpan(hour, min, 0);
+                EndTeor = DateTime.Now.Add(t);
+
+            }
+        }
+
+        public DateTime? EndFact { get; set; }
+
+        public DateTime? Prostoy { get; set; }
+        [NotMapped]
+        public string ProstoyStr
+        {
+            set
+            {
+                if (value == "-")
+                {
+                    Prostoy = null;
+                    return;
+                }
+                int.TryParse(value.Substring(0, value.IndexOf(':')), out int hour);
+                int.TryParse(value.Substring(value.IndexOf(':') + 1), out int min);
+                TimeSpan t = new TimeSpan(hour, min, 0);
+                Prostoy = DateTime.Now.Subtract(t);
+            }
+        }
+
+        public void ChangeParam(Process b)
+        {
+            this.PrefixPlav = b.PrefixPlav;
+            this.NomerPlav = b.NomerPlav;
+            this.Pech = b.Pech;
+            this.Splav = b.Splav;
+            this.Ukazanie = b.Ukazanie;
+            this.Contract = b.Contract;
+            this.NaborOg = b.NaborOg;
+            this.NaborPer = b.NaborPer;
+            this.ComplectNum = b.ComplectNum;
+            this.ComplectIzl = b.ComplectIzl;
+            this.ComplectDiam = b.ComplectDiam;
+            this.ElectVes = b.ElectVes;
+            this.Operation = b.Operation;
+            this.OperFlag = b.OperFlag;
+            this.Start = b.Start;
+            this.EndTeor = b.EndTeor;
+            this.EndFact = b.EndFact;
+            this.Prostoy = b.Prostoy;
+        }
+    }
+}
---
+++ HTMLClass.cs
@@ -0,0 +1,85 @@
+﻿using ParseService.Elements;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ParseService
+{
+    class HtmlClass
+    {
+        public List<string> GetHtml(string url)
+        {
+            List<string> html = new List<string>();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(866));
+                bool flag = false;
+                int i = 1;
+                while (!readStream.EndOfStream)
+                {
+                    string line = readStream.ReadLine();
+                    string convLine = ClearHtml(line);
+                    if (line.IndexOf("<b>") != -1)
+                    {
+                        if (!flag)
+                            flag = true;
+                    }
+                    else
+                    {
+                        if (flag)
+                        {
+                            convLine = convLine + "!";
+                            flag = false;
+                        }
+                    }
+                    if (i > 28 && i < 653)
+                        html.Add(convLine);
+                    i++;
+                }
+                response.Close();
+                readStream.Close();
+            }
+            return html;
+        }
+
+        private string ClearHtml(string line)
+        {
+            return Regex.Replace(line, "<.*?>", String.Empty).Trim();
+        }
+
+        public List<Process> ParseToProcess(List<string> processStr)
+        {
+            List<Process> processes = new List<Process>();
+            for (int i = 0; i < processStr.Count; i += 13)
+            {
+                Process process = new Process()
+                {
+                    Plavka = processStr[i],
+                    Splav = processStr[i + 1],
+                    Ukazanie = processStr[i + 2],
+                    Contract = processStr[i + 3],
+                    Nabor = processStr[i + 4],
+                    Complect = processStr[i + 5],
+                    ElectVes = int.Parse(processStr[i + 6]),
+                    Operation = processStr[i + 7],
+                    OperFlag = (processStr[i + 12] == "!") ? true : false,
+                    Start = DateTime.Parse(processStr[i + 8]),
+                    EndTeorStr = processStr[i + 9],
+                    EndFact = DateTime.Now,
+                    ProstoyStr = processStr[i + 10],
+                };
+                processes.Add(process);
+            }
+            return processes;
+        }
+    }
+}
